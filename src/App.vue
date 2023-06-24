5d2a80ca55080152a4b6c89546bf1ee3d7b2220e
KNR1997 <kethakaranasinghe@gmail.com>
2023-06-25 00:07:44 +0530

    FB: Navigation component created

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,56 @@
 <template>
-  <div>
-      <h1>Reaction Timer</h1>
-      <button @click="start" :disabled="isPlaying">Play</button>
-      <Block v-if="isPlaying" :delay ="delay"/>
+  <div class="app-wrapper">
+    <div class="app">
+      <Navigation />
+      <!-- <router-view /> -->
+    </div>
   </div>
-
 </template>
 
-
-
 <script>
-import Block from './components/Block.vue'
-
+import Navigation from './components/Navigation.vue';
 export default {
-  name: 'App',
-  components: { Block },
+  name: "app",
+  components: { Navigation },
   data() {
-    return {
-      isPlaying: false,
-      delay: null
-    }
+    return {};
   },
-  methods: {
-    start () {
-      this.isPlaying = true,
-      this.delay = 2000 + Math.random() * 5000
-    }
-  }
-}
+  created() {},
+  mounted() {},
+  methods: {},
+  watch: {},
+};
 </script>
 
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap");
 
-<style>
-#app {
-  font-family: Arial, Helvetica, sans-serif;
-  text-align: center;
-  color: #444;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Quicksand", sans-serif;
+}
+
+.app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+.container {
+  max-width: 1440px;
+  margin: 0 auto;
+}
+
+.link {
+  cursor: pointer;
+  text-decoration: none;
+  text-transform: uppercase;
+  color: black;
+}
+
+.link-light {
+  color: #fff;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
-import './assets/main.css'
+import { createApp } from "vue";
+import { createRouter, createWebHistory } from "vue-router";
+import App from "./App.vue";
+import Navigation from "./components/Navigation.vue";
 
-import { createApp } from 'vue'
-import App from './App.vue'
+const routes = [{ path: "/", name: "Home", component: Navigation }];
 
-createApp(App).mount('#app')
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+createApp(App).use(router).mount("#app");
